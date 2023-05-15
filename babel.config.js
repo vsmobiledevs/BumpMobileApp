0d13b9ab076d55d29c48d30b49465eb74bd4abe3
Viral Square Mobile Apps <103239757+dawoodabrar42@users.noreply.github.com>
2023-05-15 18:44:58 +0500

    module resolver issue fixed

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        cwd: 'babelrc',
-        root: ['./src'],
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.js',
-          '.jsx',
-          '.json',
-          '.tsx',
-          '.ts',
-          '.native.js',
-        ],
-        alias: {
-          _assets: './src/assets',
-          _components: './src/components',
-          _atoms: './src/components/atoms',
-          _molecules: './src/components/molecules',
-          _organisms: './src/components/organisms',
-          _navigations: './src/navigations',
-          _screens: './src/screens',
-          _services: './src/services',
-          _styles: './src/styles',
-          _utils: './src/utils',
-        },
-      },
-    ],
-  ],
 };
