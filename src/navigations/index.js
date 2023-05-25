2ff456bdfda6f7f419b21620d4b0f3a87a4fb920
Viral Square Mobile Apps <91531949+vsmobiledevs@users.noreply.github.com>
2023-05-25 16:25:14 +0500

    Bottom tab configer

--- src/navigations/index.js
+++ src/navigations/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
-import Splash from '../screens/Splash';
 import AuthStack from './stacks/AuthStack';
-import BottomTabScreen from './tabs/BottomTabNavigation';
 import PrivacyTerms from './stacks/PrivacyTerms';
-import BottomTabs from './tabs';
-import AuthStack from './stacks/Auth';
 import RNBootSplash from 'react-native-bootsplash';
 
 const AppStack = createStackNavigator();
@@ -17,11 +13,8 @@ const MainAppNav = () => {
       <AppStack.Navigator
         initialRouteName="auth"
         screenOptions={{headerShown: false}}>
-        {/* <AppStack.Screen name={'Splash'} component={Splash} /> */}
         <AppStack.Screen name={'auth'} component={AuthStack} />
         <AppStack.Screen name={'privacyTerms'} component={PrivacyTerms} />
-        {/* <AppStack.Screen name={'BottomTabScreen'} component={BottomTabs} /> */}
-        <AppStack.Screen name={'BottomTabScreen'} component={BottomTabScreen} />
       </AppStack.Navigator>
     </NavigationContainer>
   );
