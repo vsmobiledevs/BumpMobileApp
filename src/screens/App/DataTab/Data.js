95112fa007056fd2ac715df80bf2f2a54841f032
Viral Square Mobile Apps <91531949+vsmobiledevs@users.noreply.github.com>
2023-05-31 20:04:56 +0500

    meta mask is integrated

--- src/screens/App/DataTab/Data.js
+++ src/screens/App/DataTab/Data.js
@@ -1,7 +1,26 @@
-import {View, Text} from 'react-native';
+import {View, Text, Linking} from 'react-native';
 import React from 'react';
+import BackgroundTimer from 'react-native-background-timer';
+import MetaMaskSDK from '@metamask/sdk';
+import {ethers} from 'ethers';
+import {Provider} from 'react-redux';
 
+const sdk = new MetaMaskSDK({
+  openDeeplink: link => {
+    Linking.openURL(link);
+  },
+  timer: BackgroundTimer,
+  dappMetadata: {
+    name: 'BumpMobileApp',
+    url: 'example.com',
+  },
+});
+
+const ethereum = sdk.getProvider();
+
+const provider = new ethers.providers.Web3Provider(ethereum);
 const Data = () => {
+  console.log(provider);
   return (
     <View>
       <Text>Data</Text>
